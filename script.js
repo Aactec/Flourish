03ee01924569cf53b5fe7fb8e2e36e92cbb48cff
Aactec <aacrajitech2024@gmail.com>
2023-12-09 09:51:23 +0100

    up to date

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const navLinks = document.getElementById("navLinks");
 function showMenu() {
-    navLinks.style.right = "0";
+    navLinks.style.display = "block";
 }
 function hideMenu() {
-    navLinks.style.right = "-320px";
+    navLinks.style.display = "none";
 }
